3df78535aadfdaa82063e2800616b2de87270139
drdpatel <165212603+drdpatel@users.noreply.github.com>
2025-08-14 13:06:55 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,5 +2,6 @@
 require('./health');
 require('./userManagement');
 require('./generateNotes');
+require('./speechToken');  // ADD THIS LINE
 
-console.log('Functions loaded');
+console.log('Functions loaded: health, userManagement, generateNotes, speechToken');
