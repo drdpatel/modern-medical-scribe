8725d9dc5ea19d024af68e18f20efcd0724f52ac
drdpatel <165212603+drdpatel@users.noreply.github.com>
2025-08-15 15:50:06 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,9 @@
 require('./health');
 require('./userManagement');
 require('./generateNotes');
-require('./speechToken');  // ADD THIS LINE
+require('./speechToken');
+require('./usersSimple');  // ADD THIS for backward compatibility
+require('./patients');      // ADD THIS
+require('./visits');        // ADD THIS
 
-console.log('Functions loaded: health, userManagement, generateNotes, speechToken');
+console.log('Functions loaded: health, userManagement, generateNotes, speechToken, usersSimple, patients, visits');
